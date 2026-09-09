40465952c41376a8ea7f1fea45c807b15ddd99a7
andreas-stefik <30560627+andreas-stefik@users.noreply.github.com>
2026-09-09 16:54:00 -0700

    Added a trace capture script for mac

---
+++ Rope3DTest/capture.sh
@@ -0,0 +1,5 @@
+MTL_CAPTURE_ENABLED=1 \
+MVK_CONFIG_AUTO_GPU_CAPTURE_SCOPE=2 \
+MVK_CONFIG_AUTO_GPU_CAPTURE_OUTPUT_FILE=shadow.gputrace \
+/Applications/QuorumStudio.app/Contents/MacOS/Java/jdk/Contents/Home/bin/java \
+-XstartOnFirstThread -jar -XstartOnFirstThread Run/Default.jar
